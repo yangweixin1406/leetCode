d9e70f1137a6f09f4785748c5dd5e6fceaff8fef
yangweixin1406 <327621797@qq.com>
2020-05-06 20:20:59 +0800

    修改switch语句中的逻辑

--- easy/05.有效括号.js
+++ easy/05.有效括号.js
@@ -4,24 +4,30 @@ var isValid = function(s) {
   if (len % 2) return false
   for (let val of s) {
     switch(val) {
-      case '(':
+      case '(': {
         arr.push(val)
         break
-      case '[':
-        arr.push(val)
-        break;
-      case '{':
+      }
+      case '[': {
         arr.push(val)
         break;
-      case ')':
-        if (arr.pop() !== ')') return false
+      }
+      case '{': {
+        arr.push(val)
+        break;
+      }
+      case ')': {
+        if (arr.pop() !== '(') return false
         break
-      case ']':
-        if (arr.pop() !== ']') return false
+      }
+      case ']': {
+        if (arr.pop() !== '[') return false
         break
-      case '}':
-        if (arr.pop() !== '}') return false
+      }
+      case '}': {
+        if (arr.pop() !== '{') return false
         break
+      }
     }
   }
   return !arr.length
