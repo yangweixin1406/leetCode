02e11be61c5df85e039f65674a939d6595f17dbb
yangweixin1406 <327621797@qq.com>
2020-05-07 16:04:03 +0800

    外观数列

---
+++ easy/11.外观数列.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n
+ * @return {string}
+ * 
+ * 题解中找到的解法，厉害
+ */
+var countAndSay = function(n) {
+  let prev = '1'
+  for (let i = 1; i < n; i++) {
+    prev = prev.replace(/(\d)\1*/g, item => `${item.length}${item[0]}`)
+  }
+  return prev
+};
+
+console.log(countAndSay(5))
