a455e51c3e4008b751746625381489839418d099
yangweixin1406 <327621797@qq.com>
2020-05-09 15:35:41 +0800

    删除链表重复元素

---
+++ easy/17.删除链表里的重复元素.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ * 
+ * 链表是有头指针值，next指向下一个地址，直到为null
+ */
+var deleteDuplicates = function(head) {
+  const cur = head
+  while (cur && cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return head
+};
