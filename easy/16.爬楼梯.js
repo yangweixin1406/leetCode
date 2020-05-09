3bade512acc7cd43d06e87ddebc2978e4c62bb14
yangweixin1406 <327621797@qq.com>
2020-05-09 15:24:14 +0800

    斐波那契数列

--- easy/16.爬楼梯.js
+++ easy/16.爬楼梯.js
@@ -4,10 +4,19 @@
  * 
  * 斐波那契数列
  */
-var climbStairs = function(n) {
+var climbStairs1 = function(n) {
   const sqrt_5 = Math.sqrt(5)
   const fib_n = Math.pow((1 + sqrt_5) / 2, n + 1) - Math.pow((1 - sqrt_5) / 2, n + 1)
   return Math.round(fib_n / sqrt_5)
+};
+
+var climbStairs = function(n) {
+  const fib = []
+  fib[0] = fib[1] = 1
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2]
+  }
+  return fib[n]
 };
 
 console.log(climbStairs(3))
